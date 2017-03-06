59c6431fa98bc2d168457b9adceb6b766d261a28
Emily Long <emilylong17@gmail.com>
2017-03-06 09:41:30 -0500

    updates mar 1, 2017

--- pandas.py
+++ pandas.py
@@ -14,3 +14,21 @@ min_turnout = df_turnout.loc[df['PctCnt']==df_turnout['PctCnt'].min()]
 max_turnout = df_turnout.loc[df['PctCnt']==df_turnout['PctCnt'].max()]
 
 df_turnout_tot = df.loc[df['Contest']=='BALLOTS CAST - TOTAL']
+
+
+
+HW
+grouped = df_voters.groupby('12') # Group by party registrations
+summed = grouped['12'].count() # Get counts of each group
+summed.sort_values(inplace=True, ascending=False)
+df_summed = summed.to_frame()
+df_summed.columns = ['NUMBER']
+df_summed.index.names = ['PARTY']
+df_summed
+
+df_summed[(df_summed.index != 'D') & (df_summed.index != 'R')]
+
+parties = ['Democrat', 'Republican', 'Other']
+values = np.array([df_summed[(df_summed.index == 'D')].NUMBER.item(),df_summed[(df_summed.index == 'R')].NUMBER.item(),df_summed[(df_summed.index != 'D') & (df_summed.index != 'R')].NUMBER.sum()])
+df_parties = pd.DataFrame(values,index=parties,columns=list('N'))
+df_parties             
---
+++ week4/twitter_key.py
@@ -0,0 +1,3 @@
+api_key = 'VcdEP6VcuzmoZTS8gj2EEgb7b'
+
+api_secret = 'zRnBuXtIMlAJPvnkhFRaEAlwuGG1X5WpkfN3DVwIYNGXi4mXWn'
